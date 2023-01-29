4178dbc7fc75dffca59acf2233c986f7195eecc4
madar <madar@madars-MacBook-Pro.local>
2023-01-29 17:01:04 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -2,5 +2,7 @@ hungry = input("are you hungry")
 if hungry == "yes":
     print("eat your food")
     print('pizza')
+    print('eat burger')
+    print('eat fries')
 else:
     print("Do your homework")
